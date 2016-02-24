c1a16e7c42d6a307cbb0229c3a6dc8c9bb385dc0
Kelson Adams <kgadams93@gmail.com>
2016-02-24 13:24:49 +0000

    Update styles for footer, nav, about, home, profile forms, contact, and user index/show pages

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -17,7 +17,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
     def select_plan
       unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
-        flash[:notice] = "Please select a membership plan to sign up."
+        flash[:danger] = "Please select a membership plan to sign up."
         redirect_to root_url
       end
     end
